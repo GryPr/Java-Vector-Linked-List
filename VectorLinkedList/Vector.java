9ef455e6c1283e8348fe1dcac5cedbb95f19dc7a
Kevin <promkev@gmail.com>
2020-04-26 14:55:31 -0400

    Add VectorList manager

--- VectorLinkedList/Vector.java
+++ VectorLinkedList/Vector.java
@@ -1,9 +1,34 @@
 package VectorLinkedList;
 class Vector {
-    public int x,y,z;
+    private int x,y,z;
     public Vector(int x1, int y1, int z1){
         x = x1;
         y = y1;
+        z = z1;
+    }
+
+    // Getters/setters
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public int getZ(){
+        return z;
+    }
+
+    public void setX(int x1){
+        x = x1;
+    }
+
+    public void setY(int y1){
+        y = y1;
+    }
+
+    public void setZ(int z1){
         z = z1;
     }
 }
---
+++ VectorLinkedList/VectorList.java
@@ -0,0 +1,16 @@
+package VectorLinkedList;
+
+public class VectorList {
+    private List start;
+    public VectorList(List s){
+        start = s;
+    }
+
+    public List getStart(){
+        return start;
+    }
+
+    public void setStart(List s){
+        start = s;
+    }
+}
