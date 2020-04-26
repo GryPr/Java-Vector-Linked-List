838d11a1aae877ff9a8a67409faea4e8a5f7afd7
Kevin <promkev@gmail.com>
2020-04-26 15:19:36 -0400

    Add getIndexVector

--- VectorLinkedList/List.java
+++ VectorLinkedList/List.java
@@ -1,23 +1,38 @@
 package VectorLinkedList;
 
 public class List {
+    static private int currentIndex = 1;
+    private int index;
     private List previous, next;
     private Vector vec;
 
     // Append mode
-    public List(List p, Vector v){
-        vec = v;
+    public List(List p, int x, int y, int z){
+        vec = new Vector(x,y,z);
         previous = p;
+        index = currentIndex;
+        currentIndex++;
+        next = null;
     }
 
     // Insert mode
-    public List(List p, List n, Vector v){
-        vec = v;
+    public List(List p, List n, int x, int y, int z){
+        vec = new Vector(x,y,z);
         previous = p;
         next = n;
+        index = currentIndex;
+        currentIndex++;
     }
 
     // Setters/getters
+    public int getIndex(){
+        return index;
+    }
+
+    public void setIndex(int i){
+        index = i;
+    }
+
     public List getPrevious(){
         return previous;
     }
--- VectorLinkedList/VectorList.java
+++ VectorLinkedList/VectorList.java
@@ -13,4 +13,23 @@ public class VectorList {
     public void setStart(List s){
         start = s;
     }
+
+    public void appendVector(){
+
+    }
+
+    public List getIndexedVector(int in){
+        List next = start.getNext();
+        // Looping through the linked list
+        while (next != null){
+            // If index is found return
+            if (next.getIndex() == in){
+                return next;
+            }
+            // Else, move to the next item
+            next = next.getNext();
+        }
+        // Returns null if not found
+        return null;
+    }
 }
