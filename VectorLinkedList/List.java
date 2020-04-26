4bbed412a359902028980fb7dc00c00d11ed77ad
Kevin <promkev@gmail.com>
2020-04-26 14:51:04 -0400

    Add constructors and setters/getters

--- VectorLinkedList/List.java
+++ VectorLinkedList/List.java
@@ -1,11 +1,44 @@
 package VectorLinkedList;
 
 public class List {
-    List previous, next;
-    Vector vec;
+    private List previous, next;
+    private Vector vec;
+
+    // Append mode
+    public List(List p, Vector v){
+        vec = v;
+        previous = p;
+    }
+
+    // Insert mode
     public List(List p, List n, Vector v){
         vec = v;
         previous = p;
         next = n;
+    }
+
+    // Setters/getters
+    public List getPrevious(){
+        return previous;
+    }
+
+    public List getNext(){
+        return next;
+    }
+
+    public Vector getVector(){
+        return vec;
+    }
+
+    public void setPrevious(List p){
+        previous = p;
+    }
+
+    public void setNext(List n){
+        next = n;
+    }
+
+    public void setVector(Vector v){
+        vec = v;
     }
 }
